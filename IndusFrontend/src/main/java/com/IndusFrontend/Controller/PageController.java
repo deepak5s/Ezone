04c75716f520ec56766852c6d955f4e35d996f74
deepak5s <import.deepak@protonmail.com>
2017-07-28 14:30:31 +0530

    Completed the interface theme and other issues.

--- IndusFrontend/src/main/java/com/IndusFrontend/Controller/PageController.java
+++ IndusFrontend/src/main/java/com/IndusFrontend/Controller/PageController.java
@@ -9,8 +9,25 @@ public class PageController {
 @RequestMapping(value={"/","/home","/index"})
 public ModelAndView index(){
 	ModelAndView mv=new ModelAndView("page");
-	mv.addObject("greeting", "welcome");
+	mv.addObject("title", "Home");
+	mv.addObject("userClickHome", true);
 	return mv;
 	
 }
+@RequestMapping(value={"/about"})
+public ModelAndView about(){
+	ModelAndView mv=new ModelAndView("page");
+	mv.addObject("title", "Aboutus");
+	mv.addObject("userClickAbout", true);
+	return mv;
+	
+}
+@RequestMapping(value={"/contact"})
+public ModelAndView contact(){
+	ModelAndView mv=new ModelAndView("page");
+	mv.addObject("title", "Contactus");
+	mv.addObject("userClickContact", true);
+	return mv;
+}
+
 }
