1a804e2c5aa2c9f1981b3f6f08f07aac77d66fd5
deepak5s <import.deepak@protonmail.com>
2017-08-06 11:45:07 +0530

    SOME MODIFIACATION IN BACKEND

--- IndusFrontend/src/main/java/com/IndusFrontend/Controller/PageController.java
+++ IndusFrontend/src/main/java/com/IndusFrontend/Controller/PageController.java
@@ -1,15 +1,21 @@
 package com.IndusFrontend.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.Indusbackend.dao.CategoryDAO;
+
 @Controller
 public class PageController {
+	@Autowired
+	private CategoryDAO categoryDAO; 
 @RequestMapping(value={"/","/home","/index"})
 public ModelAndView index(){
 	ModelAndView mv=new ModelAndView("page");
 	mv.addObject("title", "Home");
+	mv.addObject("categories", categoryDAO.list());
 	mv.addObject("userClickHome", true);
 	return mv;
 	
